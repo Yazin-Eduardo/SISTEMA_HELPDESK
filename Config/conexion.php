5936497978280e56684e4a4ad653c5895527310d
Yazin-Eduardo <yazin_eduardo.potmail.com>
2023-08-06 12:37:54 -0600

    Versión 0.0.4

---
+++ Config/conexion.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+    class Conectar{
+        protected $dbh;
+
+        //FUNCIONALIDAD DE LA CADENA DE CONEXIÓN A LA BASE DE DATOS EN FORMA LOCAL NOMBRE DE LA BASE DE DATOS sistema_helpdesk
+        protected function Conexion(){
+            try {
+                $conectar = $this->dbh =  new PDO("mysql:local=localhost;dbname=sistema_helpdesk", "root", "");
+                return $conectar;
+            } catch (Exception $e) {
+                print " ¡Error DB!: " . $e->getMessage() . "<br/>";
+                die();
+            }
+        }
+
+        //FUNCIÓN UTF8 PARA NO TENER COMPILICACIÓN EN EL IDIOMA ESPAÑOL POR LAS TILDES Y LA LETRA Ñ
+        public function set_name(){
+        return $this->dbh->query("SET NAME 'utf8'");
+        }
+
+        //VALIDADCIÓN DE LA RUTA DEL PROYECTO
+        public function ruta(){
+            return "http://localhost/SISTEMA_HELPDESK/";
+        }
+    }
+?>
