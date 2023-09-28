40453eb1a51358df827935f26afcef4081269381
Yazin-Eduardo <yazin_eduardo.potmail.com>
2023-09-28 10:30:09 -0600

    Versión 0.0.8

--- View/NuevoTicket/nuevoticket.js
+++ View/NuevoTicket/nuevoticket.js
@@ -1,5 +1,21 @@
+// propiedades del summernote para quitar la configuración flotante//
 $(document).ready(function() {
     $('#tick_descrip').summernote({
-        height: 150
+        height: 150,
+        lang: "es_ES",
+        popover: {
+            image: [],
+            link: [],
+            air: [],
+        },
+        callbacks:{
+            onImageUpload: function(image){
+                console.log("Image detect...");
+                myimagetreat(image[0]);
+            },
+            onpaste: function(e){
+                console.log("Text detect...");
+            }
+        }
     });
 });
